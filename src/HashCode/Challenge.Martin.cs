8d04ba4014689eec33cdef57c0a68a1bfad99df1
Martin Kromkamp <m.kromkamp@gmail.com>
2019-02-28 20:09:54 +0000

    Next best two

--- src/HashCode/Challenge.Martin.cs
+++ src/HashCode/Challenge.Martin.cs
@@ -6,10 +6,76 @@ namespace HashCode
 {
     public partial class Challenge
     {
+        public Solution CurrentBestTwo()
+        {
+            var score = 0;
+            var remainingPhotos = Photos;
+            var slides = new List<Slide>();
+            
+            Slide currentSlide = null;
+
+            var horizontalPhoto = remainingPhotos.FirstOrDefault(p => p.Orientation == Orientation.Horizontal);
+            if (horizontalPhoto != null)
+            {
+                currentSlide = Slide.Create(horizontalPhoto);
+            }
+            else
+            {
+                currentSlide = Slide.Create(remainingPhotos.ElementAt(0), remainingPhotos.ElementAt(1));
+            }
+
+            slides.Add(currentSlide);            
+            currentSlide.Photos.ForEach(p => remainingPhotos.Remove(p));
+            
+            while (remainingPhotos.Any())
+            {
+                Slide next = null;
+                var nextInterest = -1;
+
+                foreach (var photo in remainingPhotos.Take(100))
+                {
+                    var nextPossible = Slide.Create(photo);
+
+                    if (photo.Orientation == Orientation.Vertical)
+                    {
+                        if (remainingPhotos.Count == 1)
+                            break;
+
+                        var otherVertical = remainingPhotos.FirstOrDefault(otherPhoto => otherPhoto.Id != photo.Id && otherPhoto.Orientation == Orientation.Vertical);
+                        if (otherVertical != null)
+                            nextPossible.Photos.Add(otherVertical);
+                        else
+                            continue;
+                    }
+
+                    var interest = currentSlide.GetInterestFactor(nextPossible);
+                    if (interest > nextInterest)
+                    {
+                        next = nextPossible;
+                        nextInterest = interest;
+                    }
+                }
+
+                score = score + currentSlide.GetInterestFactor(next);
+                slides.Add(next);
+                currentSlide = next;
+                next.Photos.ForEach(p => remainingPhotos.Remove(p));
+
+                Console.WriteLine($"Current slides: {slides.Count}");
+            }
+
+            Console.WriteLine(score);
+
+            return new Solution
+            {
+                Slides = slides,
+            };
+        }
+
         public Solution CurrentBestThree()
         {
             var score = 0;
-            var remainingPhotos = Photos.OrderByDescending(p => p.Tags.Count).ToList();
+            var remainingPhotos = Photos;
             var slides = new List<Slide>();
             
             Slide currentSlide = null;
--- src/HashCode/Program.cs
+++ src/HashCode/Program.cs
@@ -11,13 +11,17 @@ namespace HashCode
         {
             for (int i = 1; i < 6; i++)
             {
-                if (i != 3)
+                if (i != 3 || i != 2)
                 {
                     var inputLines = File.ReadAllLines($"input/sample{i}.in").ToArray();
                     var challenge = Input.Parse(inputLines);
                     Output.Write(challenge.CurrentBest(), $"output/sample{i}.out");
                 }
             }
+
+            var inputLines2 = File.ReadAllLines($"input/sample2.in").ToArray();
+            var challenge2 = Input.Parse(inputLines2);
+            Output.Write(challenge2.CurrentBestTwo(), $"output/sample2.out");
 
             var inputLines3 = File.ReadAllLines($"input/sample3.in").ToArray();
             var challenge3 = Input.Parse(inputLines3);
