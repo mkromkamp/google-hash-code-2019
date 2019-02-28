56736496e93b73765f5145e81540ccd59954bc57
James Lappin <james.lappin@checkout.com>
2019-02-28 18:18:16 +0000

    input parsed

--- test/HashCode.Tests/InputTests.cs
+++ test/HashCode.Tests/InputTests.cs
@@ -18,10 +18,18 @@ H 2 garden cat
         [Fact]
         public void InputExampleCorrectlyParses()
         {
-            var inputLines = ExampleInput.Split('\n').ToList();
+            var inputLines = ExampleInput.Split('\n').ToArray();
             var actual = Input.Parse(inputLines);
 
             Assert.NotNull(actual);
+            Assert.Equal(actual.Photos.Count, 4);
+
+            var photo = actual.Photos.Last();
+            Assert.Equal(photo.Id, 3);
+            Assert.Equal(photo.Orientation, Orientation.Horizontal);
+            Assert.Equal(photo.Tags.Count, 2);
+            Assert.Equal(photo.Tags.First(), "garden");
+            Assert.Equal(photo.Tags.Last(), "cat");
         }
     }
 }
