470aa51ef73203db8fb3611abb375a9d80e02c79
Martin Kromkamp <m.kromkamp@gmail.com>
2019-02-28 20:56:10 +0000

    best new

--- src/HashCode/Program.cs
+++ src/HashCode/Program.cs
@@ -15,7 +15,7 @@ namespace HashCode
 
             var inputLines2 = File.ReadAllLines($"input/sample2.in").ToArray();
             var challenge2 = Input.Parse(inputLines2);
-            Output.Write(challenge2.CurrentBestTake(150), $"output/sample2.out");
+            Output.Write(challenge2.CurrentBestTake(250), $"output/sample2.out");
 
             var inputLines3 = File.ReadAllLines($"input/sample3.in").ToArray();
             var challenge3 = Input.Parse(inputLines3);
@@ -23,11 +23,11 @@ namespace HashCode
 
             var inputLines4 = File.ReadAllLines($"input/sample4.in").ToArray();
             var challenge4 = Input.Parse(inputLines4);
-            Output.Write(challenge4.CurrentBestTake(150), $"output/sample4.out");
+            Output.Write(challenge4.CurrentBestTake(250), $"output/sample4.out");
 
             var inputLines5 = File.ReadAllLines($"input/sample5.in").ToArray();
             var challenge5 = Input.Parse(inputLines5);
-            Output.Write(challenge5.CurrentBestTake(150), $"output/sample5.out");
+            Output.Write(challenge5.CurrentBestTake(250), $"output/sample5.out");
 
             Console.WriteLine("Team Endor go!!");
         }
