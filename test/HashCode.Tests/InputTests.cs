be8a9184cb85e3f79355b72e01034fa977fc772e
James Lappin <james.lappin@checkout.com>
2019-02-28 14:40:38 +0000

    Adding in default input test

---
+++ test/HashCode.Tests/InputTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace HashCode.Tests
+{
+    public class InputTests
+    {
+        private const string ExampleInput = @"";
+
+        [Fact]
+        public void InputExampleCorrectlyParses()
+        {
+            var inputLines = ExampleInput.Split(Environment.NewLine).ToList();
+            var actual = Input.Parse(inputLines);
+
+            Assert.NotNull(actual);
+        }
+    }
+}
