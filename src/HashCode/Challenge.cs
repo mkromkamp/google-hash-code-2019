d79e657ab9371dfe83e43397aa573501baba85c1
Martin Kromkamp <m.kromkamp@gmail.com>
2019-02-28 18:30:26 +0000

    stuff

--- src/HashCode/Challenge.cs
+++ src/HashCode/Challenge.cs
@@ -9,6 +9,36 @@ namespace HashCode
 
         public Solution SolveSimple()
         {
+            var remainingPhotos = Photos;
+            var slides = new List<Slide>();
+
+            var currentSlide = Slide.Create(remainingPhotos.First(p => p.Orientation == Orientation.Horizontal));
+            slides.Add(currentSlide);
+            
+            remainingPhotos.Remove(currentSlide.Photos.First()); // <- should work :P
+            
+            while (remainingPhotos.Any())
+            {
+                Slide nextHighest = null;
+                var interestFactor = 0;
+                
+                foreach (var photo in remainingPhotos)
+                {
+                    // if (photo.Orientation == Orientation.Vertical);
+
+                    var possibleNext = Slide.Create(photo);
+                    var interest = currentSlide.GetInterestFactor(possibleNext);
+                    if (interest > interestFactor)
+                    {
+                        nextHighest = possibleNext;
+                        interestFactor = interest;
+                    }
+                }
+
+                slides.Add(nextHighest);
+                nextHighest.Photos.ForEach(p => remainingPhotos.Remove(p));
+            }
+
             return new Solution
             {
             };
--- src/HashCode/Solution.cs
+++ src/HashCode/Solution.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HashCode
 {
@@ -10,5 +11,37 @@ namespace HashCode
     public class Slide
     {
         public List<Photo> Photos { get; set; }
+
+        public static Slide Create(Photo photo)
+        {
+            return new Slide{
+                Photos = new List<Photo>{photo}
+            };
+        }
+
+        public static Slide Create(Photo photo1, Photo photo2)
+        {
+            return new Slide{
+                Photos = new List<Photo>{photo1, photo2}
+            };
+        }
+
+        public int GetInterestFactor(Slide other)
+        {
+            var thisTags = Photos.SelectMany(p => p.Tags);
+            var otherTags = other.Photos.SelectMany(p => p.Tags);
+
+            var lowest = thisTags.Intersect(otherTags).Count();
+            
+            var thisExceptOther = thisTags.Except(otherTags).Count();
+            if (thisExceptOther < lowest)
+                lowest = thisExceptOther;
+
+            var otherExceptThis = otherTags.Except(thisTags).Count();
+            if (otherExceptThis < lowest)
+                lowest = otherExceptThis;
+
+            return lowest;
+        }
     }
 }
